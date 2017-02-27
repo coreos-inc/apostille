8c356d897fe02b223c931f64bc327ee2712f1f55
Evan Cordell <cordell.evan@gmail.com>
2017-02-27 11:33:25 -0500

    refactor(storage): remove SignerStore

--- storage/memory.go
+++ storage/memory.go
@@ -2,11 +2,8 @@
 package storage
 
 import (
-	"sync"
-
 	notaryStorage "github.com/docker/notary/server/storage"
 	"github.com/docker/notary/tuf"
-	"github.com/docker/notary/tuf/data"
 	"github.com/docker/notary/tuf/signed"
 )
 
@@ -18,41 +15,4 @@ func NewAlternateRootMemStorage(cs signed.CryptoService, repo tuf.Repo, signerSt
 		repo,
 		signerStore,
 	}
-}
-
-// SignerMemoryStore stores a map of Signers to GUNs
-type SignerMemoryStore struct {
-	lock    sync.Mutex
-	signers map[SignerKey]struct{}
-}
-
-// NewSignerMemoryStore creates an empty SignerMemoryStore
-func NewSignerMemoryStore() *SignerMemoryStore {
-	return &SignerMemoryStore{
-		signers: make(map[SignerKey]struct{}),
-	}
-}
-
-// AddUserAsSigner adds a user to the signing group for a GUN
-func (st *SignerMemoryStore) AddUserAsSigner(user Username, gun data.GUN) error {
-	st.lock.Lock()
-	defer st.lock.Unlock()
-	st.signers[SignerKey{user, gun}] = struct{}{}
-	return nil
-}
-
-// RemoveUserAsSigner removes a user from the signing group for a GUN
-func (st *SignerMemoryStore) RemoveUserAsSigner(user Username, gun data.GUN) error {
-	st.lock.Lock()
-	defer st.lock.Unlock()
-	delete(st.signers, SignerKey{user, gun})
-	return nil
-}
-
-// IsSigner returns whether or not a user is in the group of signers for a GUN
-func (st *SignerMemoryStore) IsSigner(user Username, gun data.GUN) bool {
-	st.lock.Lock()
-	defer st.lock.Unlock()
-	_, ok := st.signers[SignerKey{user, gun}]
-	return ok
 }
--- storage/sql.go
+++ storage/sql.go
@@ -4,67 +4,9 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/Sirupsen/logrus"
 	notaryStorage "github.com/docker/notary/server/storage"
 	"github.com/docker/notary/tuf/data"
-	"github.com/jinzhu/gorm"
 )
-
-var SignerTableName = func() string {
-	return "signers"
-}
-
-// Signers maps a signing user to a gun
-type Signer struct {
-	gorm.Model
-	Gun    string `sql:"type:varchar(255);not null"`
-	Signer string `sql:"type:varchar(255);not null"`
-}
-
-// TableName sets a specific table name for TUFFile
-func (g Signer) TableName() string {
-	return SignerTableName()
-}
-
-// SignerSQLStorage stores signer data in a SQL db
-type SignerSQLStorage struct {
-	gorm.DB
-}
-
-// NewSignerSQLStorage stores signer data in an existing DB
-func NewSignerSQLStorage(gormDB gorm.DB) *SignerSQLStorage {
-	return &SignerSQLStorage{
-		DB: gormDB,
-	}
-}
-
-// AddUserAsSigner adds a user to the signing group for a GUN
-func (db *SignerSQLStorage) AddUserAsSigner(user Username, gun data.GUN) error {
-	if user == "" {
-		logrus.Info("not adding user as signer, username empty")
-		return nil
-	}
-	q := db.Create(&Signer{Gun: string(gun), Signer: string(user)})
-	return q.Error
-}
-
-// RemoveUserAsSigner removes a user from the signing group for a GUN
-func (db *SignerSQLStorage) RemoveUserAsSigner(user Username, gun data.GUN) error {
-	signer := Signer{Signer: string(user), Gun: string(gun)}
-	q := db.Unscoped().Where(&signer).Delete(Signer{})
-	return q.Error
-}
-
-// IsSigner returns whether or not a user is in the group of signers for a GUN
-func (db *SignerSQLStorage) IsSigner(user Username, gun data.GUN) bool {
-	var row Signer
-	q := db.Where(&Signer{Gun: string(gun), Signer: string(user)}).Limit(1).First(&row)
-	if q.Error != nil {
-		logrus.Infof("couldn't find %s as a signer for %s: %v", user, gun, q.Error)
-		return false
-	}
-	return true
-}
 
 // NamespacedSQLStorage sets alternate table names and proxies to a standard SQLStorage
 // See server/storage/sqldb.go
