ef77a3b47c7441ea1da61a448e6d6b5ab0464015
Evan Cordell <cordell.evan@gmail.com>
2017-03-03 07:57:23 -0500

    refactor(sql): remove unneeded tables

--- migrations/mysql/0001_initial.up.sql
+++ migrations/mysql/0001_initial.up.sql
@@ -6,25 +6,11 @@ CREATE TABLE `tuf_files` (
 	  `gun` varchar(255) NOT NULL,
 	  `role` varchar(255) NOT NULL,
 	  `version` int(11) NOT NULL,
+    `namespace` VARCHAR(255) DEFAULT "published",
 	  `data` longblob NOT NULL,
 	  `sha256` CHAR(64) DEFAULT NULL,
 	  PRIMARY KEY (`id`),
-	  UNIQUE KEY `gun` (`gun`,`role`,`version`),
-	  INDEX `sha256` (`sha256`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `alternate_tuf_files` (
-	  `id` int(11) NOT NULL AUTO_INCREMENT,
-	  `created_at` timestamp NULL DEFAULT NULL,
-	  `updated_at` timestamp NULL DEFAULT NULL,
-	  `deleted_at` timestamp NULL DEFAULT NULL,
-	  `gun` varchar(255) NOT NULL,
-	  `role` varchar(255) NOT NULL,
-	  `version` int(11) NOT NULL,
-	  `data` longblob NOT NULL,
-	  `sha256` CHAR(64) DEFAULT NULL,
-	  PRIMARY KEY (`id`),
-	  UNIQUE KEY `gun` (`gun`,`role`,`version`),
+	  UNIQUE KEY `gun` (`gun`,`role`,`version`, `namespace`),
 	  INDEX `sha256` (`sha256`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -41,36 +27,14 @@ CREATE TABLE `changefeed` (
     `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
     `gun` varchar(255) NOT NULL,
     `version` int(11) NOT NULL,
+    `namespace` VARCHAR(255) DEFAULT "published",
     `sha256` CHAR(64) DEFAULT NULL,
     `category` VARCHAR(20) NOT NULL DEFAULT "update",
     PRIMARY KEY (`id`),
     FOREIGN KEY (`category`) REFERENCES `change_category` (`category`),
-    INDEX `idx_changefeed_gun` (`gun`)
+    INDEX `idx_changefeed_gun_ns` (`gun`, `namespace`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
-CREATE TABLE `alternate_changefeed` (
-    `id` int(11) NOT NULL AUTO_INCREMENT,
-    `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
-    `gun` varchar(255) NOT NULL,
-    `version` int(11) NOT NULL,
-    `sha256` CHAR(64) DEFAULT NULL,
-    `category` VARCHAR(20) NOT NULL DEFAULT "update",
-    PRIMARY KEY (`id`),
-    FOREIGN KEY (`category`) REFERENCES `change_category` (`category`),
-    INDEX `idx_changefeed_gun` (`gun`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `signers` (
-	`id`         INT(11)       NOT NULL AUTO_INCREMENT,
-	`created_at` TIMESTAMP     DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  `deleted_at` timestamp NULL DEFAULT NULL,
-	`gun` 			 VARCHAR(255)  NOT NULL,
-	`signer`     VARCHAR(255)  NOT NULL,
-	PRIMARY KEY (`id`),
-	UNIQUE KEY `gun` (`gun`,`signer`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- SHA2 function takes the column name or a string as the first parameter, and the
 -- hash size as the second argument. It returns a hex string.
