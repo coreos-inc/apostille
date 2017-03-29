816c4e3b7b2445cd734087ed5755aa543491dc75
Evan Cordell <cordell.evan@gmail.com>
2017-03-29 09:44:31 -0400

    fix(tests): fix integration tests

--- aposql/mysql-initdb.d/initial-apostille.sql
+++ aposql/mysql-initdb.d/initial-apostille.sql
@@ -6,13 +6,6 @@ GRANT
 	ALL PRIVILEGES ON `apostille`.*
 	TO "server"@"%";
 
--- Read Only user for apostille_root
-CREATE USER "readonlyuser"@"%" IDENTIFIED BY "12345";
-GRANT
-	SELECT ON `apostille_root`.*
-	TO "readonlyuser"@"%";
-
--- Write user for apostille_root
 CREATE USER "server_root"@"%" IDENTIFIED BY "54321";
 GRANT
 	ALL PRIVILEGES ON `apostille_root`.*
