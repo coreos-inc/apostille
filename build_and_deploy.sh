f0c780cb67d950d06d1f9cec8bc73283aa0fbad5
ldelossa <ldelossa.ld@gmail.com>
2020-02-11 17:29:36 -0500

    fix image tags

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -8,16 +8,16 @@ APOSTILLE_SIGNER_IMAGE="quay.io/app-sre/${BASE_SIGNER_IMG}"
 GIT_HASH=`git rev-parse --short=7 HEAD`
 
 # build and push server
-docker build -t ${APOSTILLE_SERVER_IMG}:${GIT_HASH} -f ./server.Dockerfile .
-docker tag ${APOSTILLE_SERVER_IMG}:${GIT_HASH} ${APOSTILLE_SERVER_IMG}:latest
+docker build -t ${APOSTILLE_SERVER_IMAGE}:${GIT_HASH} -f ./server.Dockerfile .
+docker tag ${APOSTILLE_SERVER_IMAGE}:${GIT_HASH} ${APOSTILLE_SERVER_IMAGE}:latest
 
 skopeo copy --dest-creds "${QUAY_USER}:${QUAY_TOKEN}" \
-    "docker-daemon:${APOSTILLE_SERVER_IMG}" \
-    "docker://${APOSTILLE_SERVER_IMG}:latest"
+    "docker-daemon:${APOSTILLE_SERVER_IMAGE}" \
+    "docker://${APOSTILLE_SERVER_IMAGE}:latest"
 
 skopeo copy --dest-creds "${QUAY_USER}:${QUAY_TOKEN}" \
-    "docker-daemon:${IMG}" \
-    "docker://${APOSTILLE_SERVER_IMG}:${GIT_HASH}"
+    "docker-daemon:${APOSTILLE_SERVER_IMAGE}" \
+    "docker://${APOSTILLE_SERVER_IMAGE}:${GIT_HASH}"
 
 # build and push signer
 docker build -t ${APOSTILLE_SIGNER_IMAGE}:${GIT_HASH} -f ./signer.Dockerfile .
